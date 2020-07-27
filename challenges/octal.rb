605ee495f03c47c3ad6cae7d65f0103d393841e5
Nicholas Pizzigati <npizzigati@gmail.com>
2020-07-27 12:47:17 -0500

    First version of octal

---
+++ challenges/octal.rb
@@ -0,0 +1,75 @@
+# Octal
+# Implement octal to decimal conversion. Given an octal input
+# string, your program should produce a decimal output.
+
+# Note:
+
+# Implement the conversion yourself. Do not use something else to
+# perform the conversion for you. Treat invalid input as octal 0.
+
+# About Octal (Base-8):
+
+# Decimal is a base-10 system.
+
+# A number 233 in base 10 notation can be understood as a linear
+# combination of powers of 10:
+
+# The rightmost digit gets multiplied by 100 = 1
+# The next number gets multiplied by 101 = 10
+# ...
+# The n*th number gets multiplied by 10n-1*.
+# All these values are summed.
+# So:
+
+# input: String
+# output: Integer
+
+# Specifications:
+# Explicit:
+# - Input is a string representation of an octal number
+# - Output is an Integer in decimal format
+# - Invalid input to be treated as octal 0
+# Implicit:
+# -
+
+# Approach: Convert input parameter to string to check
+# validity. If not valid, return 0. If negative, set negative
+# flag, and remove negative symbol. Convert do Integer. Call
+# #digits on input parameter, # which will give us digits in
+# reversed order. Multiply
+# each digit by (8^(digit index)) which is 8 to the nth
+# power, where n is the place of the digit. Return the sum of the
+# products.
+
+class Octal
+  def initialize(octal)
+    @octal_string = octal.to_s
+    if @octal_string.match?(/^-\d+$/)
+      @negative = true
+      puts @negative
+      @octal_string.sub!('-', '')
+    end
+  end
+
+  def to_decimal
+    return 0 unless @octal_string.match?(/^\d+$/)
+    digits = @octal_string.to_i.digits
+    result = 0
+    digits.each_with_index do |digit, index|
+      result += digit * (8**index)
+    end
+    @negative ? -result : result
+  end
+end
+
+#   233 # decimal
+# = 2*10^2 + 3*10^1 + 3*10^0
+# = 2*100  + 3*10   + 3*1
+# Octal is similar, but uses powers of 8 rather than powers of
+# 10. So:
+
+#   233 # octal
+# = 2*8^2 + 3*8^1 + 3*8^0
+# = 2*64  + 3*8   + 3*1
+# = 128   + 24    + 3
+# = 155
