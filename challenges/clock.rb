153ccc963c5360c30ba60405aaa0e1c10219e9a3
Nicholas Pizzigati <npizzigati@gmail.com>
2020-08-14 12:23:27 -0500

    Clock, first version

---
+++ challenges/clock.rb
@@ -0,0 +1,46 @@
+# Create a clock that is independent of date.
+
+# You should be able to add and subtract minutes to it. Two
+# clocks that represent the same time should be equal to each
+# other.
+
+# Classes:
+# Clock
+# Class methods:
+# at(Integer hour, Integer minute)
+# instantiates an object
+# to_s
+# Returns a string representation of time (see test suite)
+
+class Clock
+  MINUTES_IN_DAY = 24 * 60
+
+  def initialize(hours, minutes)
+    @minutes = (minutes + hours * 60) % MINUTES_IN_DAY
+  end
+
+  def self.at(hours, minutes = 0)
+    new(hours, minutes)
+  end
+
+  def +(other)
+    new_minutes = (@minutes + other) % MINUTES_IN_DAY
+    self.class.new(0, new_minutes)
+  end
+
+  def -(other)
+    new_minutes = @minutes - other
+    new_minutes += MINUTES_IN_DAY until new_minutes > 0
+    self.class.new(0, new_minutes)
+  end
+
+  def ==(other)
+    self.to_s == other.to_s
+  end
+
+  def to_s
+    hours, minutes = @minutes.divmod 60
+    format('%.2<hours>d:%.2<minutes>d', hours: hours,
+                                       minutes: minutes)
+  end
+end
