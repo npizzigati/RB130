6182ea72ecb2fe06c196b28df96581001c6f0754
Nicholas Pizzigati <npizzigati@gmail.com>
2020-08-22 21:32:17 -0500

    Binary search tree

---
+++ challenges/binary_search_tree.rb
@@ -0,0 +1,117 @@
+# Binary Search Tree
+# Write a program that inserts numbers and searches in a binary
+# tree.
+
+# When we need to represent sorted data, an array does not make a
+# good data structure.
+
+# Say we have the array [1, 3, 4, 5], and we add 2 to it so it
+# becomes [1, 3, 4, 5, 2] now we must sort the entire array
+# again! We can improve on this by realizing that we only need to
+# make space for the new item [1, nil, 3, 4, 5], and then adding
+# the item in the space we added. But this still requires us to
+# shift many elements down by one.
+
+# Binary Search Trees, however, can operate on sorted data much
+# more efficiently.
+
+# A binary search tree consists of a series of connected
+# nodes. Each node contains a piece of data (e.g. the number 3),
+# a variable named left, and a variable named right. The left and
+# right variables point at nil, or other nodes. Since these other
+# nodes in turn have other nodes beneath them, we say that the
+# left and right variables are pointing at subtrees. All data in
+# the left subtree is less than or equal to the current node's
+# data, and all data in the right subtree is greater than the
+# current node's data.
+
+# For example, if we had a node containing the data 4, and we
+# added the data 2, our tree would look like this:
+
+#   4
+#  /
+# 2
+# If we then added 6, it would look like this:
+
+#   4
+#  / \
+# 2   6
+# If we then added 3, it would look like this
+
+#    4
+#  /   \
+# 2     6
+#  \
+#   3
+# And if we then added 1, 5, and 7, it would look like this
+
+#       4
+#     /   \
+#    /     \
+#   2       6
+#  / \     / \
+# 1   3   5   7
+
+# Problem breakdown
+# class Bst
+# ===========
+# #initialize(Integer n)
+# Create a tree with the top node n
+# (An object with data: n, left: nil, right: nil)
+# -----------
+# #data()
+# Return the content of the receiver node
+# -----------
+# #insert(n)
+# #create a new node with data n on the left or right branch of the current
+# node, as appropriate
+# -----------
+# #each { |item| block }
+# returns enumerator if no block
+# access each element in order from left to right (smallest to
+# largest)
+# Approach:
+# Create private method to walk tree. Walk tree on call to each
+
+class Bst
+  attr_accessor :data, :left, :right
+
+  def initialize(number)
+    @data = number
+    @left = nil
+    @right = nil
+  end
+
+  def each
+    items = walk.sort
+    if block_given?
+      for item in items
+        yield(item)
+      end
+    else
+      items.each
+    end
+  end
+
+  def insert(number, current_node = self)
+    if number <= current_node.data
+      if current_node.left.nil?
+        current_node.left = current_node.class.new(number)
+      else
+        insert(number, current_node.left)
+      end
+    else
+      if current_node.right.nil?
+        current_node.right = current_node.class.new(number)
+      else
+        insert(number, current_node.right)
+      end
+    end
+  end
+
+  def walk(current_node = self, accum = [])
+    walk current_node.left, accum unless current_node.left.nil?
+    walk current_node.right, accum unless current_node.right.nil?
+    accum << current_node.data
+  end
+end
